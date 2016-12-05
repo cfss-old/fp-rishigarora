10d864a06b57228f9bd5525a6e0ac9d91e323ce8
Rishi <rishiarora313@gmail.com>
2016-12-05 00:22:40 -0600

    preliminary plots, started factor manipulation

--- analysis1.R
+++ analysis1.R
@@ -22,16 +22,48 @@ filter(facultylist, Position == "Distinguished Service Professor")
 ## preliminary plots 
 # plot number of faculty by position in each department
 ggplot(data = facultylist) +
-  geom_count(mapping = aes(x = Position, y = Department))
+  geom_count(mapping = aes(x = Position, y = Department))  
 
-# plot number of faculty by position
+# plot signatories by position
 ggplot(data = facultylist) +
   geom_bar(mapping = aes(x = Position)) +
   coord_flip() + 
-  theme_classic()  
+  theme_minimal() +
+  labs(title="Faculty Signatories by Position", y="# of Signatories")  
+ggsave("plot_byposition.png", width = 5, height = 5)
+
+# plot signatories by department 
+ggplot(data = facultylist) +
+  geom_bar(mapping = aes(x = Department)) +
+  coord_flip() + 
+  theme_minimal() +
+  labs(title="Faculty Signatories by Department", y="# of Signatories")  
+ggsave("plot_bydept.png", width = 5, height = 5)
+
+# REORDER = http://r4ds.had.co.nz/factors.html
+
+## combine positions based on level of job security
+library(forcats)
+
+tenurestatus <- facultylist %>%
+  mutate(Position = fct_collapse(Position,
+    "Has Tenure" = c("Professor Emeritus", "Professor", "Named Professor", "Distinguished Service Professor", "Executive Director", "Visiting Professor", "Associate Professor"),
+    "Tenure Track" = c("Clinical Professor", "Assistant Professor", "Assistant Clinical Professor"),
+    "Non-Tenure" = c("Visiting Lecturer", "Senior Lecturer", "Provost's Postdoctoral Scholar", "Lecturer", "Harper Fellow"
+  ))) 
+
+## combine departments based on division
+biological sciences
+humanities
+social sciences
+physical sciences 
+professional schools (law, med, ssa, )
 
 
-  
+
+
+
+
 ### group by sciences vs humanities vs social science 
 ### group by tenure no tenure (include clinical in no tenure, emeritus in tenure)
 ### experiment by language - tendency for some to participate vs not? regression
