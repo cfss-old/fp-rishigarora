d5a6a4fc6f20266e369986e81c798f45f891a258
Rishi <rishiarora313@gmail.com>
2016-12-07 22:22:16 -0600

    added dummy variables for log regression

--- analysis3.R
+++ analysis3.R
@@ -1,3 +1,5 @@
+install.packages("dummies")
+library(dummies)
 
 # load dataset of all faculty from six selected departments
 dept.rosters = read.csv("fullrosters.csv")
@@ -9,9 +11,19 @@ summary(dept.rosters)
 dept.rosters$Status <- factor(dept.rosters$Status)
 dept.rosters$Signed <- as.logical(dept.rosters$Signed)
 
-sign.tenure <- glm(Signed ~ Status, data = dept.rosters)
+# create dummy variables for tenure status
+
+dummytest <- dummy.data.frame(data = dept.rosters, names = "Status")
+
+
+# logisitic regression 
+sign.tenure <- glm(Signed ~ Status, data = dept.rosters, family = binomial)
 
 summary(sign.tenure)
 
+# cross validation 
 
+
+
+# change status to dummys 
 
